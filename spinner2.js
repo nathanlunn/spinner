a525d724722fe451b3abcee0af14696178d40c0f
Nathan Lunn <nathandouglaslunn@gmail.com>
2022-04-11 23:08:45 +0000

    added spinner2.js, a refactored version of spinner1.js

---
+++ spinner2.js
@@ -0,0 +1,7 @@
+let sentence = ['|  ', '\r/  ', '\r-  ', '\r\\  ', '\r|  ', '\r/  ', '\r-  ', '\r\\  ', '\r|  ', '\n'];
+
+for (let i = 0; i < sentence.length; i++) {
+  setTimeout(() => {
+    process.stdout.write(`${sentence[i]}`);
+  }, i * 200);
+}
